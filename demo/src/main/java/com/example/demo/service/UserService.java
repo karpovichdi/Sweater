1e81efc3f12456241ac684ae84625c449e0948bd
Karpovich Dmitry <karpovichdi@hotmail.com>
2020-04-23 12:24:23 +0300

    Added: Page for profile editing.

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -44,16 +44,7 @@ public class UserService implements UserDetailsService {
 
         userRepository.save(user);
 
-        if (!StringUtils.isEmpty(user.getEmail())){
-            String message = String.format(
-                    "Hello, %s! \n" +
-                            "Welcome to Sweater. Please, visit next link: " + baseUrl + "/activate/%s",
-                    user.getUsername(),
-                    user.getActivationCode()
-            );
-
-            mailService.send(user.getEmail(), "Activation code", message);
-        }
+        sendMessage(user);
 
         return true;
     }
@@ -70,5 +61,65 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
 
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()){
+            if(roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                                 (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged){
+            user.setEmail(email);
+
+            if (!StringUtils.isEmpty(email)){
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if (!StringUtils.isEmpty(password)){
+            user.setPassword(password);
+        }
+
+        userRepository.save(user);
+
+        if (isEmailChanged){
+            sendMessage(user);
+        }
+    }
+
+    private void sendMessage(User user) {
+        if (!StringUtils.isEmpty(user.getEmail())){
+            String message = String.format(
+                    "Hello, %s! \n" +
+                            "Welcome to Sweater. Please, visit next link: " + baseUrl + "/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode()
+            );
+
+            mailService.send(user.getEmail(), "Activation code", message);
+        }
     }
 }
