993c2904fd3e83a3d4d59e213c7a8a3afc6b429f
Karpovich Dmitry <karpovichdi@hotmail.com>
2020-04-23 22:49:56 +0300

    Fixed: Bug with ui activation message.

--- demo/src/main/java/com/example/demo/models/entities/User.java
+++ demo/src/main/java/com/example/demo/models/entities/User.java
@@ -26,10 +26,6 @@ public class User implements UserDetails {
     @NotBlank(message = "Password cannot be empty")
     private String password;
 
-    @Transient
-    @NotBlank(message = "Password confirmation cannot be empty")
-    private String password2;
-
     @Email(message = "Email is not correct")
     @NotBlank(message = "Email cannot be empty")
     private String email;
@@ -128,13 +124,5 @@ public class User implements UserDetails {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 }
